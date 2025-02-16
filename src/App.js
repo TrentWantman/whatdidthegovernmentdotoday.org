e527ed81b85b6ac7c62a10e36334b667365307cd
Trent <Wantmantrent@gmail.com>
2025-02-16 01:33:48 -0500

    Initial commit for whatdidthegovernmentdo project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>What Did the Government Do?</h1>
+      <p>Stay updated on recent government actions and policies.</p>
+    </div>
+  );
+}
+
+export default App;
